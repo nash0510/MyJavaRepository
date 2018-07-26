122e6fb66856a93fc2d3ca7bc2a2f89215d203e9
Nashville Fernandes <nashville_fernandes@persistent.co.in>
2018-07-26 15:39:14 +0530

    message improved and date displayed

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,11 +1,11 @@
 class HelloWorld{
 
 	static void displayDate(){
-		System.out.println("Display today's date:-");	
+		System.out.println("Display today's date:-"+new Date());	
 	}
 
 	public static void main(String[] args){
-		System.out.println("Hello World...Welcome to Git ");
+		System.out.println("Hello World...!!!Welcome to Git...!!!");
 		displayDate();
 
 	}
